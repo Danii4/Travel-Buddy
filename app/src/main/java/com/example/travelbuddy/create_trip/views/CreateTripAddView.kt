34a436ca1f52bbe3b3918d81bff795abbab67a5c
Pranavan <ppirahal@uwaterloo.ca>
2024-03-01 10:59:48 -0500

    setup calendar UI component

--- app/src/main/java/com/example/travelbuddy/create_trip/views/CreateTripAddView.kt
+++ app/src/main/java/com/example/travelbuddy/create_trip/views/CreateTripAddView.kt
@@ -1,6 +1,8 @@
 package com.example.travelbuddy.create_trip.views
 
+import android.util.Log
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -13,15 +15,43 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import com.maxkeppeker.sheets.core.models.base.rememberSheetState
+import com.maxkeppeler.sheets.calendar.CalendarDialog
+import com.maxkeppeler.sheets.calendar.models.CalendarConfig
+import com.maxkeppeler.sheets.calendar.models.CalendarSelection.Dates
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateTripAddView() {
-    var value by remember { mutableStateOf("Type Here") }
+
+    // Calendar Component
+    // From: https://www.youtube.com/watch?v=uAw87DdUnxg
+    val calendarState = rememberSheetState()
+
+    CalendarDialog(
+        state = calendarState,
+        config = CalendarConfig(
+            monthSelection = true,
+            yearSelection = true,
+        ),
+        selection = Dates{dates ->
+            Log.d("SelectedDate", "$dates")
+        }
+    )
+
+    Button (onClick = {
+        calendarState.show()
+    }) {
+        Text (text = "Pick Date Range")
+    }
+
+    // Trip Notes
+    // From: https://developer.android.com/jetpack/compose/text/user-input
+    var note by remember { mutableStateOf("Type Here") }
 
     TextField(
-        value = value,
-        onValueChange = { value = it },
+        value = note,
+        onValueChange = { note = it },
         label = { Text("Enter Notes") },
         maxLines = 2,
         textStyle = TextStyle(color = Color.Black,),
