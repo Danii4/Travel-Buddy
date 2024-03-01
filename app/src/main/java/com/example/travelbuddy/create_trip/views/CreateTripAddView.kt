71c4970377dee65660c0dbaf5f365a222b1fe5ef
Pranavan <ppirahal@uwaterloo.ca>
2024-03-01 18:12:42 -0500

    added search bar to add view

--- app/src/main/java/com/example/travelbuddy/create_trip/views/CreateTripAddView.kt
+++ app/src/main/java/com/example/travelbuddy/create_trip/views/CreateTripAddView.kt
@@ -1,13 +1,20 @@
 package com.example.travelbuddy.create_trip.views
 
 import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -25,10 +32,28 @@ import com.maxkeppeker.sheets.core.models.base.rememberSheetState
 import com.maxkeppeler.sheets.calendar.CalendarDialog
 import com.maxkeppeler.sheets.calendar.models.CalendarConfig
 import com.maxkeppeler.sheets.calendar.models.CalendarSelection.Dates
+import androidx.compose.material3.SearchBar
+import androidx.compose.runtime.mutableStateListOf
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateTripAddView() {
+    var destBarText by remember {
+        mutableStateOf("")
+    }
+    var destBarActive by remember {
+        mutableStateOf(false)
+    }
+    val calendarState = rememberSheetState()
+    var destNote by remember { mutableStateOf("Type Here") }
+    var destList = remember {
+        mutableStateListOf(
+            "Sintra",
+            "Lisbon",
+            "Porto",
+            "Madeira"
+        )
+    }
 
     Scaffold (){paddingValues ->
         Column(
@@ -37,9 +62,59 @@ fun CreateTripAddView() {
                 .padding(paddingValues),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            // Destination Bar
+            SearchBar(
+                modifier = Modifier.fillMaxWidth(),
+                query = destBarText,
+                onQueryChange = {
+                                destBarText = it
+                },
+                onSearch = {
+                           destBarActive = false
+                },
+                active = destBarActive,
+                onActiveChange = {
+                    destBarActive = it
+                },
+                placeholder = {
+                    Text(text = "Search for destination ...")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
+                },
+                trailingIcon = {
+                    if (destBarActive){
+                        Icon(
+                            modifier = Modifier.clickable {
+                                if (destBarText.isNotEmpty()){
+                                    destBarText = ""
+                                } else {
+                                    destBarActive = false
+                                }
+                            },
+                            imageVector = Icons.Default.Close,
+                            contentDescription = "Search Icon"
+                        )
+                    }
+                }
+            ) {
+                destList.forEach {
+                    Row (modifier = Modifier.padding(all = 14.dp)){
+                        Icon(
+                            modifier = Modifier.padding(end = 10.dp),
+                            imageVector = Icons.Default.Place,
+                            contentDescription = "Location"
+                        )
+                        Text(text = it)
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
             // Calendar Component
             // From: https://www.youtube.com/watch?v=uAw87DdUnxg
-            val calendarState = rememberSheetState()
 
             CalendarDialog(
                 state = calendarState,
@@ -62,11 +137,10 @@ fun CreateTripAddView() {
 
             // Trip Notes
             // From: https://developer.android.com/jetpack/compose/text/user-input
-            var note by remember { mutableStateOf("Type Here") }
 
             TextField(
-                value = note,
-                onValueChange = { note = it },
+                value = destNote,
+                onValueChange = { destNote = it },
                 label = { Text("Enter Notes") },
                 maxLines = 2,
                 textStyle = TextStyle(color = Color.Black,),
