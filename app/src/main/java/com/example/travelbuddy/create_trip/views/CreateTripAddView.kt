a8ad2f66ee805031a31685e122a0d40482fd5fb2
Pranavan <ppirahal@uwaterloo.ca>
2024-03-02 18:29:20 -0500

    added icons to destination

--- app/src/main/java/com/example/travelbuddy/create_trip/views/CreateTripAddView.kt
+++ app/src/main/java/com/example/travelbuddy/create_trip/views/CreateTripAddView.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -61,8 +62,14 @@ fun GenerateDestinationView(destination: DestinationModel.Destination) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp)
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            Icon(
+                imageVector = Icons.Filled.Place,
+                contentDescription = null,
+                modifier = Modifier.size(24.dp),
+            )
             Box(
                 modifier = Modifier
                     .weight(0.6f)
