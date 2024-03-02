cf01a08b63e2052489fe7286942b86eb3b3de1e0
Pranavan <ppirahal@uwaterloo.ca>
2024-03-02 12:26:41 -0500

    updated destination model

--- app/src/main/java/com/example/travelbuddy/data/model/DestinationModel.kt
+++ app/src/main/java/com/example/travelbuddy/data/model/DestinationModel.kt
@@ -3,9 +3,9 @@ package com.example.travelbuddy.data.model
 import java.time.LocalDate
 
 class DestinationModel {
-    data class Metadata (
-        val destName: String = "",
-        val destStartDate: LocalDate,
-        val destEndDate: LocalDate,
+    data class Metadata(
+        val Name: String = "",
+        val startDate: LocalDate,
+        val endDate: LocalDate,
     )
 }
