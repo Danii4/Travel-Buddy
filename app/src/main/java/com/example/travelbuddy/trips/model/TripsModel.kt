02d1d82908df7c61ab1baebf7b69f401323f6af6
Pranavan <ppirahal@uwaterloo.ca>
2024-03-16 14:33:24 -0400

    updated trips model to take padding values

--- app/src/main/java/com/example/travelbuddy/trips/model/TripsModel.kt
+++ app/src/main/java/com/example/travelbuddy/trips/model/TripsModel.kt
@@ -1,6 +1,7 @@
 package com.example.travelbuddy.trips.model
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 
 data class TripsModel(val id: Int, val title: String, val date: String)
-data class TripAddPageModel(val page: @Composable () -> Unit, val metadata: String = "")
+data class TripAddPageModel(val page: @Composable (paddingVal: PaddingValues) -> Unit, val metadata: String = "")
