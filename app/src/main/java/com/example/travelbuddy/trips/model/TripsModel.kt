1975718091508c3717d065f6cf6c950fe1500665
Pranavan <ppirahal@uwaterloo.ca>
2024-03-16 12:50:27 -0400

    added horizontal pager

--- app/src/main/java/com/example/travelbuddy/trips/model/TripsModel.kt
+++ app/src/main/java/com/example/travelbuddy/trips/model/TripsModel.kt
@@ -1,4 +1,6 @@
 package com.example.travelbuddy.trips.model
 
-data class TripsModel(val id: Int, val title: String, val date: String) {
-}
+import androidx.compose.runtime.Composable
+
+data class TripsModel(val id: Int, val title: String, val date: String)
+data class TripAddPageModel(val title: String = "Test", val content: @Composable () -> Unit)
--- app/src/main/java/com/example/travelbuddy/trips/views/TripsView.kt
+++ app/src/main/java/com/example/travelbuddy/trips/views/TripsView.kt
@@ -2,10 +2,8 @@
 
 package com.example.travelbuddy.trips.views
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,14 +14,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Flight
 import androidx.compose.material.icons.outlined.AttachMoney
-import androidx.compose.material.icons.outlined.List
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.MaterialTheme
@@ -31,9 +32,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -47,14 +48,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupPositionProvider
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import com.example.travelbuddy.create_trip.CreateTripAddViewModel
-import com.example.travelbuddy.expenses.ExpensesViewModel
 import com.example.travelbuddy.expenses.add_edit_expense.AddEditExpenseViewModel
-import java.time.LocalDate
+import com.example.travelbuddy.trips.model.TripAddPageModel
+import com.example.travelbuddy.create_trip.views.CreateTripAddView
 
 class TripsViewModel : ViewModel() {
     var trips = mutableStateListOf<String>()
@@ -162,6 +164,7 @@ fun TripsView(
         var isSheetOpen by rememberSaveable {
             mutableStateOf(false)
         }
+        var showPager by remember { mutableStateOf(false) }
 
         Box(
             modifier = Modifier.fillMaxSize(),
@@ -170,6 +173,7 @@ fun TripsView(
             Button(
                 onClick = {
                     isSheetOpen = true
+                    showPager = true
                 },
                 modifier = Modifier
                     .fillMaxWidth()
@@ -181,70 +185,132 @@ fun TripsView(
                 Text(text = "New Trip")
             }
         }
-        if (isSheetOpen) {
-            ModalBottomSheet(
-                sheetState = sheetState,
-                onDismissRequest = {
-                    isSheetOpen = false
-                }
-            )
-            {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(paddingValues),
-                ) {
-                    Text(
-//            text = stringResource(id = R.string.medication_name),
-                        text = "Title",
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                    Spacer(modifier = Modifier.height(10.dp))
-
-                    TextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        value = tripTitle,
-                        onValueChange = { tripTitle = it },
-//            placeholder = { Text(text = "e.g. Hexamine") },
-                    )
-                    Spacer(modifier = Modifier.height(20.dp))
-
-
-                    Row(
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 10.dp)
+        if (showPager) {
+            val titlePage = TripAddPageModel(
+                title = "Include Trip Name",
+                content = {
+                    ElevatedCard(
+                        modifier = Modifier.fillMaxWidth(0.5f)
                     ) {
-
-                        Box(
-                            Modifier
-                                .weight(1f)
-                                .padding(start = 8.dp),
-                        ) {
-                            Button(
-                                onClick = {
-                                    tripTitleActive = false
-                                    if (tripTitle.isNotBlank()) {
-                                        tripList.add(tripTitle)
-                                        tripTitle = ""
-                                        isSheetOpen=false
-                                    }
-                                },
-                                modifier = Modifier.fillMaxWidth(),
-                                shape = RoundedCornerShape(10.dp),
-                                colors = ButtonDefaults.buttonColors(
-                                    containerColor = Color(0xff5cb85c)
-                                )
-                            ) {
-                                Text("Confirm")
-                            }
-                        }
+                        TextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = tripTitle,
+                            onValueChange = { tripTitle = it },
+                        )
                     }
                 }
+            )
+            val addDestPage = TripAddPageModel(
+                title = "",
+                content = { CreateTripAddView() }
+            )
+            val tripsAddScreenList = listOf(titlePage, addDestPage)
+            GenerateTripAddViews(tripsAddScreenList)
+        }
+//        if (isSheetOpen) {
+//            ModalBottomSheet(
+//                sheetState = sheetState,
+//                onDismissRequest = {
+//                    isSheetOpen = false
+//                }
+//            )
+//            {
+//                Column(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .padding(paddingValues),
+//                ) {
+//                    Text(
+////            text = stringResource(id = R.string.medication_name),
+//                        text = "Title",
+//                        style = MaterialTheme.typography.bodyLarge
+//                    )
+//                    Spacer(modifier = Modifier.height(10.dp))
+//
+//                    TextField(
+//                        modifier = Modifier.fillMaxWidth(),
+//                        value = tripTitle,
+//                        onValueChange = { tripTitle = it },
+////            placeholder = { Text(text = "e.g. Hexamine") },
+//                    )
+//                    Spacer(modifier = Modifier.height(20.dp))
+//
+//
+//                    Row(
+//                        horizontalArrangement = Arrangement.SpaceBetween,
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(horizontal = 10.dp)
+//                    ) {
+//
+//                        Box(
+//                            Modifier
+//                                .weight(1f)
+//                                .padding(start = 8.dp),
+//                        ) {
+//                            Button(
+//                                onClick = {
+//                                    tripTitleActive = false
+//                                    if (tripTitle.isNotBlank()) {
+//                                        tripList.add(tripTitle)
+//                                        tripTitle = ""
+//                                        isSheetOpen=false
+//                                    }
+//                                },
+//                                modifier = Modifier.fillMaxWidth(),
+//                                shape = RoundedCornerShape(10.dp),
+//                                colors = ButtonDefaults.buttonColors(
+//                                    containerColor = Color(0xff5cb85c)
+//                                )
+//                            ) {
+//                                Text("Confirm")
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun GenerateTripAddViews(pages: List<TripAddPageModel>) {
+    val pagerState = rememberPagerState(pageCount = { pages.size })
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .background(Color.White)
+    ) {
+        HorizontalPager(state = pagerState) { index ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(text = pages[index].title, modifier = Modifier.padding(bottom = 16.dp))
+                pages[index].content()
+            }
+        }
+        Row(
+            Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 8.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pagerState.pageCount) { iteration ->
+                val color =
+                    if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(16.dp)
+                )
             }
         }
     }
-
-
 }
