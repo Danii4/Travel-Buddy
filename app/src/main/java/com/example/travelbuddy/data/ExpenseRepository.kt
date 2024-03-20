74511f5bc6e54a2426e72dc111a3d716511205e5
Abbiram <a6ramana@uwaterloo.ca>
2024-03-19 22:38:49 -0400

    add expense repo file

--- app/src/main/java/com/example/travelbuddy/data/ExpenseRepository.kt
+++ app/src/main/java/com/example/travelbuddy/data/ExpenseRepository.kt
@@ -1,4 +1,30 @@
 package com.example.travelbuddy.data
 
+import com.example.travelbuddy.data.model.ExpenseModel
+import com.example.travelbuddy.data.model.ResponseModel
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
 class ExpenseRepository {
+    private var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    suspend fun addExpense(expense: ExpenseModel.Expense): ResponseModel.Response {
+        return try {
+            val colRef = db.collection("expense").add(
+                mapOf(
+                    "name" to expense.name,
+                    "type" to expense.type,
+                    "amount" to expense.amount,
+                    "date" to expense.date
+                )
+            ).await()
+            ResponseModel.Response.Success
+        } catch (e: Exception) {
+            ResponseModel.Response.Failure(
+                error = e.message ?: "Error adding an expense. Please try again."
+            )
+        }
+    }
+
+
 }
