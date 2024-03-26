ee943ebb56cc27e5b8bc6fef61a15a3a40a70e47
Pranavan <ppirahal@uwaterloo.ca>
2024-03-26 18:47:51 -0400

    update destination repository function to include uid in payload

--- app/src/main/java/com/example/travelbuddy/data/DestinationRepositoryImpl.kt
+++ app/src/main/java/com/example/travelbuddy/data/DestinationRepositoryImpl.kt
@@ -1,9 +1,9 @@
 package com.example.travelbuddy.data
 
-import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import com.example.travelbuddy.data.model.DestinationModel
 import com.example.travelbuddy.data.model.ResponseModel
+import com.example.travelbuddy.repository.AuthRepository
 import com.example.travelbuddy.repository.DestinationRepository
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
@@ -11,23 +11,39 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 
-class DestinationRepositoryImpl @Inject constructor() :  DestinationRepository {
+class DestinationRepositoryImpl @Inject constructor(
+    private val authRepository: AuthRepository
+) :  DestinationRepository {
     private var db: FirebaseFirestore = FirebaseFirestore.getInstance()
-
-    override suspend fun addDestination(destination: DestinationModel.Destination) : ResponseModel.Response {
-        return try {
+    override suspend fun addDestination(destination: DestinationModel.Destination) : ResponseModel.Response? {
+        return authRepository.getUserId()?.let { uid ->
             val destinationLoad = hashMapOf(
                 "name" to destination.name,
                 "startDate" to destination.startDate,
                 "endDate" to destination.endDate,
+                "uid" to uid,
             )
             val firestoreDb = Firebase.firestore
-            firestoreDb.collection("destinations").add(
-                destinationLoad
-            ).await()
-            ResponseModel.Response.Success
-        } catch (e: Exception){
-            ResponseModel.Response.Failure(e.message ?: "Error adding destination")
+            try {
+                firestoreDb.collection("destinations").add(destinationLoad).await()
+                ResponseModel.Response.Success
+            } catch (e: Exception){
+                ResponseModel.Response.Failure(e.message ?: "Error adding destination")
+            }
         }
+//        return try {
+//            val destinationLoad = hashMapOf(
+//                "name" to destination.name,
+//                "startDate" to destination.startDate,
+//                "endDate" to destination.endDate,
+//            )
+//            val firestoreDb = Firebase.firestore
+//            firestoreDb.collection("destinations").add(
+//                destinationLoad
+//            ).await()
+//            ResponseModel.Response.Success
+//        } catch (e: Exception){
+//            ResponseModel.Response.Failure(e.message ?: "Error adding destination")
+//        }
     }
 }
