e20ae1de87824a99443b2bf31b46aefd73ce7428
Pranavan <ppirahal@uwaterloo.ca>
2024-03-27 14:51:11 -0400

    moved local delete and add functionality to viewModel

--- app/src/main/java/com/example/travelbuddy/trips/add_trips/AddTripsViewModel.kt
+++ app/src/main/java/com/example/travelbuddy/trips/add_trips/AddTripsViewModel.kt
@@ -6,7 +6,11 @@ import androidx.navigation.NavController
 import com.example.travelbuddy.Screen
 import com.example.travelbuddy.data.DestinationRepositoryImpl
 import com.example.travelbuddy.data.model.DestinationModel
+import com.example.travelbuddy.trips.add_trips.model.AddTripsPageModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,12 +18,40 @@ import javax.inject.Inject
 class AddTripsViewModel @Inject constructor(
     private val destinationRepositoryImpl: DestinationRepositoryImpl
 ) : ViewModel() {
-    fun submitDestination(destination: DestinationModel.Destination) {
+    private val _state = MutableStateFlow(AddTripsPageModel.AddTripViewState())
+    val state: StateFlow<AddTripsPageModel.AddTripViewState>
+        get() = _state
+
+    private val destinationList: MutableStateFlow<List<DestinationModel.Destination>> = MutableStateFlow(listOf())
+    private val dummy : MutableStateFlow<Boolean> = MutableStateFlow(_state.value.dummy)
+
+    init {
         viewModelScope.launch {
-            destinationRepositoryImpl.addDestination(
-                destination
-            )
+            combine(destinationList, dummy) {
+                    destinationList: List<DestinationModel.Destination>,
+                    dummy: Boolean ->
+                AddTripsPageModel.AddTripViewState(
+                    destinationList = destinationList,
+                    dummy = dummy,
+                )
+            }.collect {
+                _state.value = it
+            }
         }
+    }
+
+
+    fun addDestination(destination: DestinationModel.Destination) {
+//        viewModelScope.launch {
+//            destinationRepositoryImpl.addDestination(
+//                destination
+//            )
+//        }
+        destinationList.value += destination
+    }
+
+    fun deleteDestination(destination: DestinationModel.Destination){
+        destinationList.value -= destination
     }
     fun navigateToCreateTripAdd(navController: NavController) {
         navController.navigate(Screen.TripAdd.route)
