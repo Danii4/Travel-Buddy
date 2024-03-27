171fab9e3b70b5c97265d7c0ff0445e266376182
Pranavan <ppirahal@uwaterloo.ca>
2024-03-27 11:35:25 -0400

    rename AddTripsModel to AddTripsPageModel

--- app/src/main/java/com/example/travelbuddy/trips/add_trips/model/AddTripsModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.travelbuddy.trips.add_trips.model
-
-import com.example.travelbuddy.data.model.DestinationModel
-
-class DestinationPageModel {
-    data class DestinationViewState(
-        val destinationList: MutableList<DestinationModel.Destination>
-    )
-}
