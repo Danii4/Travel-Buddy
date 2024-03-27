9f113daceabfdc49e308e584790d8ee86a39295b
Suvasan Krishnasamy <suvasan19@gmail.com>
2024-03-27 18:45:43 -0400

    setup itinerary

---
+++ app/src/main/java/com/example/travelbuddy/itinerary/ItineraryViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.travelbuddy.itinerary
+
+class ItineraryViewModel {
+}
--- app/src/main/java/com/example/travelbuddy/itinerary/model/ItineraryModel.kt
+++ app/src/main/java/com/example/travelbuddy/itinerary/model/ItineraryModel.kt
@@ -1,6 +1,7 @@
 package com.example.travelbuddy.itinerary.model
 data class ItineraryModel(val id: Int, val items: List<ItineraryItem>) {
-    class ItineraryItem(val id: Int, val title: String, val date: String) {
+}
 
-    }
+class ItineraryItem(val id: Int, val title: String, val date: String) {
+
 }
