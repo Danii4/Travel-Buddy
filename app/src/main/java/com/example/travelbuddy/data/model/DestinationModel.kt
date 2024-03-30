2db12ef83de6ea90447e0b72783a6e196f17fef6
Pranavan <ppirahal@uwaterloo.ca>
2024-03-30 13:09:26 -0400

    added type casting to Date in AddEditDestinationView and DestinationModel

--- app/src/main/java/com/example/travelbuddy/data/model/DestinationModel.kt
+++ app/src/main/java/com/example/travelbuddy/data/model/DestinationModel.kt
@@ -1,11 +1,12 @@
 package com.example.travelbuddy.data.model
 
-import java.time.LocalDate
+import java.util.Date
 
 class DestinationModel {
     data class Destination(
+        val id: String = "",
         val name: String = "",
-        val startDate: LocalDate,
-        val endDate: LocalDate,
+        val startDate: Date,
+        val endDate: Date,
     )
 }
