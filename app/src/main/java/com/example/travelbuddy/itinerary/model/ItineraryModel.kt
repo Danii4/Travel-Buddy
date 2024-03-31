f8c6c8b5ec1fd5bd9d8a938b4a643ba435359dc9
Pranavan <ppirahal@uwaterloo.ca>
2024-03-30 21:18:49 -0400

    updated ItinieraryModel naming

--- app/src/main/java/com/example/travelbuddy/itinerary/model/ItineraryModel.kt
+++ app/src/main/java/com/example/travelbuddy/itinerary/model/ItineraryModel.kt
@@ -2,7 +2,7 @@ package com.example.travelbuddy.itinerary.model
 
 import com.example.travelbuddy.data.model.ItineraryModel
 
-class ItineraryModel {
+class ItineraryPageModel {
     data class ItineraryViewState(
         val itineraryList: List<ItineraryModel.Itinerary> = emptyList(),
         val isLoading: Boolean = false,
