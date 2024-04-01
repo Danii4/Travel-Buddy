796a269ff0ec6626820089dea2529add6657b577
Pranavan <ppirahal@uwaterloo.ca>
2024-04-01 06:56:29 -0400

    revamped onboarding UI

--- app/src/main/java/com/example/travelbuddy/trips/add_trips/views/AddTripNameView.kt
+++ app/src/main/java/com/example/travelbuddy/trips/add_trips/views/AddTripNameView.kt
@@ -1,24 +1,29 @@
 package com.example.travelbuddy.trips.add_trips.views
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Route
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import com.example.travelbuddy.languageTranslation.CustomColors
 import com.example.travelbuddy.trips.add_trips.AddTripsViewModel
 
 
@@ -29,31 +34,46 @@ fun AddTripNameView() {
 
     Column(
         modifier = Modifier
+            .background(color = CustomColors.Indigo)
             .fillMaxSize()
             .padding(),
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom,
     ) {
-        Box(modifier = Modifier.padding(vertical = 90.dp)){
-            Icon(
-                imageVector = Icons.Default.Route,
-                contentDescription = "Info Icon",
-                modifier = Modifier
-                    .size(100.dp)
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            AsyncImage(
+                model = "https://i.ibb.co/WsdSCcS/pngwing-com.png",
+                contentDescription = "Airplane"
             )
         }
-        Text(
-            text = "Enter Trip Name",
-            modifier = Modifier.padding(10.dp)
-        )
-        ElevatedCard(
-            modifier = Modifier.fillMaxWidth(0.75f)
+        Column(
+            modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(30.dp, 30.dp, 0.dp, 0.dp)
+                )
+                .padding(50.dp),
+            verticalArrangement = Arrangement.spacedBy(25.dp)
         ) {
-            TextField(
+            Text(
+                textAlign = TextAlign.Left,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                text = "Name your Adventure",
+                lineHeight = 35.sp,
+            )
+            OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = state.tripName,
                 onValueChange = { viewModel.setTripName(it) },
-                singleLine = true
+                singleLine = true,
+                shape = RoundedCornerShape(15.dp)
             )
+            Spacer(modifier = Modifier.height(25.dp))
         }
     }
 }
