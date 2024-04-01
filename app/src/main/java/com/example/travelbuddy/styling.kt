8add735018237f0cfeea63765176c2a0dcd2f12a
“Dania <dhanif@uwaterloo.ca>
2024-04-01 12:53:59 -0400

    updated styling

--- app/src/main/java/com/example/travelbuddy/styling.kt
+++ app/src/main/java/com/example/travelbuddy/styling.kt
@@ -10,7 +10,6 @@ object CustomColors {
     var Indigo = Color(0xFF36558F)
     var LightIndigo = Color(0xFF6D86B3)
     var Pink = Color(0xFFED7B84)
-    var LightPink = Color(0xFFE5C0C3)
     var LightRed = Color(255, 100, 109)
 }
 
